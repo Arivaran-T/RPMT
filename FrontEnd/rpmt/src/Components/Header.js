1449c22a2fdedcfa853f8ee8716825b3f3f07a78
Nirojan21 <nirojan.yoga@gmail.com>
2022-03-27 12:47:37 +0530

    header develpment finished

--- FrontEnd/rpmt/src/Components/Header.js
+++ FrontEnd/rpmt/src/Components/Header.js
@@ -9,22 +9,31 @@ import {
   Badge,
   Menu,
   MenuItem,
+  Tooltip,
+  Button,
 } from "@mui/material";
 import AccountCircle from "@mui/icons-material/AccountCircle";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
-
+import LightModeIcon from "@mui/icons-material/LightMode";
 import MailIcon from "@mui/icons-material/Mail";
+
 import logo from "../Assets/logo.png";
 
-function Header() {
+function Header(props) {
   const [auth, setAuth] = useState(true);
   const [anchorEl, setAnchorEl] = useState(null);
+  const [mode, setMode] = useState("light");
+
   const handleClose = () => {
     setAnchorEl(null);
   };
+
+  props.handler(mode);
+
   const handleMenu = (event) => {
     setAnchorEl(event.currentTarget);
   };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar color={"primary"} position="sticky">
@@ -38,32 +47,64 @@ function Header() {
             aria-haspopup="true"
             // onClick={handleMenu}
             color="inherit"
+            onClick={() => {
+              setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+              props.handler(mode);
+            }}
           >
-            <DarkModeIcon onClick={() => {}} fontSize="inherit" />
+            {mode === "light" ? (
+              <DarkModeIcon fontSize="inherit" />
+            ) : (
+              <LightModeIcon fontSize="inherit" />
+            )}
           </IconButton>
-          <IconButton
-            size="large"
-            aria-label="show 4 new mails"
-            color="inherit"
-          >
-            <Badge badgeContent={1} color="error">
-              <MailIcon fontSize="inherit" />
-            </Badge>
-          </IconButton>
+          {/* before auth */}
+          {!auth && (
+            <Box sx={{ flexGrow: 0, display: { xs: "flex", md: "flex" } }}>
+              <Button
+                key={"login"}
+                // onClick={handleCloseNavMenu}
+                color="secondary"
+                // sx={{ my: 2, display: "block" }}
+              >
+                Log In
+              </Button>
+              <Button
+                key={"login"}
+                // onClick={handleCloseNavMenu}
+                color="secondary"
+                // sx={{ my: 2, display: "block" }}
+              >
+                Sign up
+              </Button>
+            </Box>
+          )}
 
           {/*user profile*/}
           {auth && (
             <div>
               <IconButton
                 size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
+                aria-label="show 4 new mails"
                 color="inherit"
               >
-                <AccountCircle fontSize="inherit" />
+                <Badge badgeContent={1} color="error">
+                  <MailIcon fontSize="inherit" />
+                </Badge>
               </IconButton>
+
+              <Tooltip title="profile">
+                <IconButton
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleMenu}
+                  color="inherit"
+                >
+                  <AccountCircle fontSize="inherit" />
+                </IconButton>
+              </Tooltip>
               <Menu
                 color="#e28743"
                 sx={{ mt: "45px" }}
@@ -82,7 +123,13 @@ function Header() {
                 onClose={handleClose}
               >
                 <MenuItem onClick={handleClose}>Profile</MenuItem>
-                <MenuItem onClick={()=>{setAuth(false)}}>Log Out</MenuItem>
+                <MenuItem
+                  onClick={() => {
+                    setAuth(false);
+                  }}
+                >
+                  Log Out
+                </MenuItem>
               </Menu>
             </div>
           )}
